26cb5ab1b90eaa443133157bfa3d5dc4646f7f79
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-03-30 16:09:02 +0200

    likes and save buttons

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createPost() {
         author: createPerson(),
 
         location: faker.location.country(),
-        content: faker.image.url({ width: 640, height: Math.round(4 + Math.random() * 3)*120}),
+        content: faker.image.url({ width: 640, height: Math.round(4 + Math.random() * 2)*120}),
         description: faker.lorem.sentences({ min: 1, max: 3 })
     }
 }
@@ -42,6 +42,9 @@ const postAuthorUsername = document.querySelector('.post-author-name');
 const postLocation = document.querySelector('.post-author-location');
 const postContent = document.querySelector('.post-content');
 
+const likeButton = document.getElementById('likeButton');
+const saveButton = document.getElementById('saveButton');
+
 const postDescriptionUsername = document.getElementById('postDescriptionUsername');
 const postDescription = document.getElementById('postDescription');
 
@@ -58,6 +61,26 @@ const postCommentContainer = document.querySelector('.post-comments-container');
 const postCommentsSection = document.querySelector('.comments-section');
 
 setUpPost();
+
+likeButton.addEventListener('click', (e) => {
+    const button = e.target;
+    hitLike(button);
+})
+
+saveButton.addEventListener('click', (e) => {
+    const button = e.target;
+    if (button.classList.contains('pressed')) return;
+    button.classList.toggle('black')
+    button.classList.add('pressed');
+    setTimeout(() => {button.classList.remove('pressed')},500);
+})
+
+function hitLike(button) {
+    if (button.classList.contains('pressed')) return;
+    button.classList.toggle('red')
+    button.classList.add('pressed');
+    setTimeout(() => {button.classList.remove('pressed')},500);
+}
 
 //Open likes section
 postLikesButton.addEventListener('click', () => {
@@ -189,7 +212,8 @@ function setUpComment(comment, parent) {
     commentMessage.classList.add('comment-message');
     commentMessage.innerText = comment.message;
     commentLike.classList.add('comment-like', 'side-x-mar-sm', 'point');
-    commentLike.innerHTML = `<svg aria-label="Like" class="x1lliihq x1n2onr6 xyb1xck" fill="gray" height="24" role="img" viewBox="0 0 24 24" width="20"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>`;
+    commentLike.innerHTML = `<svg aria-label="Like" height="22" role="img" viewBox="0 0 48 48" width="18"><title>Unlike</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg></div>`;
+    commentLike.addEventListener('click',(e) => {hitLike(e.target)});
     commentReply.classList.add('comment-reply-button', 'gray-text', 'bold', 'f-tiny');
     commentReply.innerText = 'Reply';
 
